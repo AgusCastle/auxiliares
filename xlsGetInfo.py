9af0a6a45ca5d875c1f6b3efd70ed29c69a2f9bc
Agus Castillo <agu13xd@hotmail.com>
2022-09-20 13:01:12 -0500

    XLS

---
+++ xlsGetInfo.py
@@ -0,0 +1,44 @@
+import xlrd
+import shutil
+
+archivo = '/Users/agustincastillo/Documents/Documentacion/Annotation_Base11.xls'
+
+wb = xlrd.open_workbook(archivo)
+hoja = wb.sheet_by_index(0)
+
+names = []
+grades = []
+
+for fila in range(1, hoja.nrows):
+    names.append(str(hoja.cell_value(fila, 0)))
+    grades.append(int(hoja.cell_value(fila, 2)))
+
+
+archivo = '/Users/agustincastillo/Documents/Documentacion/Annotation_Base12.xls'
+
+wb = xlrd.open_workbook(archivo)
+hoja = wb.sheet_by_index(0)
+
+for fila in range(1, hoja.nrows):
+    names.append(str(hoja.cell_value(fila, 0)))
+    grades.append(int(hoja.cell_value(fila, 2)))
+
+
+archivo = '/Users/agustincastillo/Documents/Documentacion/Annotation_Base13.xls'
+
+wb = xlrd.open_workbook(archivo)
+hoja = wb.sheet_by_index(0)
+
+for fila in range(1, hoja.nrows):
+    names.append(str(hoja.cell_value(fila, 0)))
+    grades.append(int(hoja.cell_value(fila, 2)))
+
+
+carpetaOrigen = '/Vídeos/Datasets Retina/drive-download-20220712t015923Z-001/Messidor/Base1/'
+carpetaDestino = '/Documentos/datsets-dr/messidor/'
+
+for name, grad in zip(names, grades):
+    origen = r'' + carpetaOrigen + name
+    destino = r'' + carpetaDestino + str(grad) + '/' + name
+
+    shutil.move(origen, destino)
