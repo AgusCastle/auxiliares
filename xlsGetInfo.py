eb486d15abda551f7b4bedd2c87a2e25792ca05b
Agus Castillo <agu13xd@hotmail.com>
2022-09-20 14:08:47 -0500

    XLS

--- xlsGetInfo.py
+++ xlsGetInfo.py
@@ -2,7 +2,7 @@
 import xlrd
 import shutil
 
-archivo = './../Annotation Base21.xls'
+archivo = './../Annotation Base31.xls'
 
 wb = xlrd.open_workbook(archivo)
 hoja = wb.sheet_by_index(0)
@@ -15,7 +15,7 @@ for fila in range(1, hoja.nrows):
     grades.append(int(hoja.cell_value(fila, 2)))
 
 print(len(names))
-archivo = './../Annotation Base22.xls'
+archivo = './../Annotation Base32.xls'
 
 wb = xlrd.open_workbook(archivo)
 hoja = wb.sheet_by_index(0)
@@ -25,7 +25,7 @@ for fila in range(1, hoja.nrows):
     grades.append(int(hoja.cell_value(fila, 2)))
 
 
-archivo = './../Annotation Base23.xls'
+archivo = './../Annotation Base33.xls'
 
 wb = xlrd.open_workbook(archivo)
 hoja = wb.sheet_by_index(0)
@@ -34,7 +34,7 @@ for fila in range(1, hoja.nrows):
     names.append(str(hoja.cell_value(fila, 0)))
     grades.append(int(hoja.cell_value(fila, 2)))
 
-archivo = './../Annotation Base24.xls'
+archivo = './../Annotation Base34.xls'
 
 wb = xlrd.open_workbook(archivo)
 hoja = wb.sheet_by_index(0)
@@ -44,7 +44,7 @@ for fila in range(1, hoja.nrows):
     grades.append(int(hoja.cell_value(fila, 2)))
 
 
-carpetaOrigen = '/home/bringascastle/Vídeos/datasets-retina/drive-download-20220712T015923Z-001/Messidor/Base2/'
+carpetaOrigen = '/home/bringascastle/Vídeos/datasets-retina/drive-download-20220712T015923Z-001/Messidor/Base3/'
 carpetaDestino = '/home/bringascastle/Documentos/datasets-dr/messidor/'
 i = 0
 for name, grad in zip(names, grades):
