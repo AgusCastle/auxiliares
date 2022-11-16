503c3fb6653d14c0d52feb071fbd4afc98c50023
AgusCastle <agu13xd@hotmail.com>
2022-11-15 18:50:13 -0600

    Imagenes de entrenamiento

--- generarMatrizConfusion.py
+++ generarMatrizConfusion.py
@@ -47,52 +47,81 @@ def loadInfo(filename: str, array: str):
     return data[array]
 
 
-def grafLoss(fln1, fln2):
+def grafLoss(fln1, fln2='', fln3='', fln4='', fln5=''):
     data1 = loadInfo(fln1, 'loss')
-    data2 = loadInfo(fln2, 'loss')
+    # data2 = loadInfo(fln2, 'loss')
+    # data3 = loadInfo(fln3, 'loss')
+    # data4 = loadInfo(fln4, 'loss')
+    # data5 = loadInfo(fln5, 'loss')
 
     fig, ax = plt.subplots()
-    epocas = range(1, len(data1) + 1, 1)
+    epocas = range(0, 100, 1)
 
     ax.plot(epocas, data1, marker='o')
-    ax.plot(epocas, data2, marker='o')
-    plt.legend(('ResNet', 'ConvNext'),
+    # ax.plot(epocas, data2, marker='o')
+    # ax.plot(epocas, data3, marker='o')
+    # ax.plot(epocas, data4, marker='o')
+    # ax.plot(epocas, data5, marker='o')
+    
+    plt.legend(('ConvNext'),
                prop={'size': 10}, loc='upper right')
 
     plt.ylabel('Loss')
     plt.xlabel('Epocas')
-    plt.title('Perdida con el set de train de kaggle')
+    plt.title('Perdida con el set de train de DDR desde Imagenet')
     plt.show()
 
 
-def grafAcc(fln1, fln2):
+def grafAcc(fln1, fln2='', fln3='', fln4='', fln5=''):
     data1 = loadInfo(fln1, 'stats')
     data2 = loadInfo(fln2, 'stats')
+    data3 = loadInfo(fln3, 'stats')
+    data4 = loadInfo(fln4, 'stats')
+    data5 = loadInfo(fln5, 'stats')
 
     fig, ax = plt.subplots()
-    print(len(data1))
-    print(len(data2))
-    epocas = range(1, len(data1) + 1, 1)
+    epocas = range(28, 100, 1)
 
-    arrays = {0: [], 1: []}
+    arrays = {0: [], 1: [], 2:[], 3:[], 4:[]}
 
     for obj1, obj2 in zip(data1, data2):
-        res, conv = obj1['acc'], obj2['acc']
+        res, conv = obj1['mild'], obj2['mild']
         arrays[0].append(res)
         arrays[1].append(conv)
 
+    for obj1, obj2 in zip(data3, data4):
+        res, conv = obj1['mild'], obj2['mild']
+        arrays[2].append(res)
+        arrays[3].append(conv)
+
+    for obj1 in data5:
+        res = obj1['mild']
+        arrays[4].append(res)
+
     ax.plot(epocas, arrays[0], marker='o')
     ax.plot(epocas, arrays[1], marker='o')
+    ax.plot(epocas, arrays[2], marker='o')
+    ax.plot(epocas, arrays[3], marker='o')
+    ax.plot(epocas, arrays[4], marker='o')
 
-    plt.legend(('ResNet', 'ConvNext'),
+    plt.legend(('ConvNext 0.2', 
+                'ConvNext 0.4',
+                'ConvNext 0.6',
+                'ConvNext 0.8',
+                'ConvNext 0.05'),
                prop={'size': 10}, loc='lower right')
 
     plt.ylabel('acc')
     plt.xlabel('Epocas')
-    plt.title('Acc con el set de valid de kaggle')
+    plt.title('Acc con clase Mild con el set de valid de DDR')
     plt.show()
 
-
+grafAcc('/home/bringascastle/Documentos/repos/retinopatia-diabetica-dl/runs/ddr_convnext_decay_0.2/info_train_convnext.json',
+'/home/bringascastle/Documentos/repos/retinopatia-diabetica-dl/runs/ddr_convnext_decay_0.4/info_train_convnext.json',
+'/home/bringascastle/Documentos/repos/retinopatia-diabetica-dl/runs/ddr_convnext_lr_2/info_train_convnext.json',
+'/home/bringascastle/Documentos/repos/retinopatia-diabetica-dl/runs/ddr_convnext_decay_0.8/info_train_convnext.json',
+'/home/bringascastle/Documentos/repos/retinopatia-diabetica-dl/runs/ddr_convnext_decay_0.05/info_train_convnext.json')
+#grafLoss('/home/bringascastle/Documentos/repos/retinopatia-diabetica-dl/runs/ddr_imagenet_convnext/result_convnext.json')
 def graficaApInTrain(filename: str):
 
     data = loadInfo(filename, 'stats')
@@ -124,6 +153,6 @@ def graficaApInTrain(filename: str):
 
 
 # graficaApInTrain('JSONFiles/convnext/info_train_convnext.json')
-# grafAcc('JSONFiles/resnet/info_train_resnet.json',
-#         'JSONFiles/convnext/info_train_convnext.json')
-matrizConfucion()
+#grafAcc('/home/bringascastle/Documentos/repos/retinopatia-diabetica-dl/runs/ddr_resnet_3/info_train_resnet.json',
+#        '/home/bringascastle/Documentos/repos/retinopatia-diabetica-dl/runs/ddr_convnext_3/info_train_convnext.json')
+#matrizConfucion()
